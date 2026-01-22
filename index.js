abf8a0e549d66e5da49aa7cdd7638805031b4c82
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-22 20:19:23 +0000

    nujei;ab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const MAX_CAPACITY = 500;
+
+
+
+document.getElementById("result").innerHTML = MAX_CAPACITY;
+
+
+//should create a constant named MAX_CAPACITY and set it to 500
+//should assign the innerHTML of the HTML element with the id result 
+// to MAX_CAPACITY
 
